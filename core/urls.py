0d3f7c62cbc8976f0a6d32ab5f1e923ff4c85c70
vshalsah29 <vshalsah29@gmail.com>
2026-04-22 03:38:22 +0530

    first commit

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hotel import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('book/<int:room_id>/', views.book_room, name='book_room'),
+    
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
